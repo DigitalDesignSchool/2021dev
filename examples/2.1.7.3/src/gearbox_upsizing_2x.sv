945d9c979c80abaeacc7aa64deff762c241ff16d
dsmv <dsmekhov@irq.ru>
2021-06-22 15:03:41 +0300

    Работа над direct test

--- examples/2.1.7.3/src/gearbox_upsizing_2x.sv
+++ examples/2.1.7.3/src/gearbox_upsizing_2x.sv
@@ -13,10 +13,29 @@ module gearbox_upsizing_2x
   input  wire             in_tvalid,
   output wire             in_tready,
 
-  output wire[nb - 1:0]   out_tdata,
+  output wire [nb*2- 1:0] out_tdata,
   output wire             out_tvalid,
   input  wire             out_tready
 );
+
+logic             flag_hf;
+logic [nb-1:0]    reg_hf;
+
+always @(posedge aclk)
+  if( ~aresetn )
+    flag_hf <= #1 '0;
+  else if( in_tvalid & in_tready )
+    flag_hf <= #1 ~flag_hf;
+
+always  @(posedge aclk) 
+  if( in_tvalid & in_tready & ~flag_hf )
+    reg_hf <= #1 in_tdata;
+
+assign out_tdata = { reg_hf, in_tdata };
+
+assign in_tready = ~flag_hf | out_tready;
+
+assign out_tvalid = flag_hf & in_tvalid;
 
 
 endmodule
--- examples/2.1.7.3/src/testbench.sv
+++ examples/2.1.7.3/src/testbench.sv
@@ -4,7 +4,7 @@
 `default_nettype none
 
 module tb
-  #( parameter test_id=1
+  #( parameter test_id=0
    )
   ();
   
@@ -37,17 +37,18 @@ localparam  n = 5, nb = n * 8;
 logic              aclk=0;
 logic              aresetn;
 
-logic [nb - 1:0]   in_tdata;
-logic              in_tvalid;
+logic [nb - 1:0]   in_tdata='0;
+logic              in_tvalid='0;
 logic              in_tready;
 
-logic [nb - 1:0]   out_tdata;
+logic [nb*2 - 1:0] out_tdata;
 logic              out_tvalid;
 logic              out_tready;
   
-int               cnt_ok=0;  
-int               cnt_error=0;
-logic             test_done=0;
+int                 cnt_wr=0;
+int                 cnt_ok=0;  
+int                 cnt_error=0;
+logic               test_done=0;
 initial begin  
 //   int fd = $fopen("test_id.txt", "r");
 //   $fscanf( fd,"%d\n",test_id);
@@ -75,6 +76,56 @@ gearbox_upsizing_2x   uut
           .*
 );
 
+task write_data;
+    input logic [nb - 1:0]    data;
+    input int                 pause;  // 0 - tvalid still high
+begin
+      in_tdata  <= #1 data;
+      in_tvalid <= #1 '1;
+      @(posedge aclk iff in_tvalid & in_tready);
+
+      if( cnt_wr<16 ) begin
+        $display( "input: %s  (%h)", data, data );
+      end
+
+      if( pause>0 ) begin
+        in_tvalid <= #1 '0;
+        in_tdata  <= #1 '0;
+        for( int ii=0; ii<pause; ii++ )
+          @(posedge aclk);
+      end
+
+end endtask
+
+generate
+  case( test_id)
+    0: begin
+
+        initial begin
+
+            out_tready = 1;
+
+            #500;
+            @(posedge aclk);
+            write_data( "ABCDE", 0 );
+            write_data( "FGHIJ", 16 );
+
+            
+            write_data( "ABCDE", 8 );
+            write_data( "FGHIJ", 8 );
+
+            #100;
+
+            test_done=1;
+        end
+
+    end
+
+    1: begin
+
+    end
+  endcase
+endgenerate
 
 
 endmodule

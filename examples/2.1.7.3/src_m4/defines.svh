a28431991318a707a8fd6d970a2f4d8434a6efc6
MaximInnopolis <m.latypov@innopolis.university>
2021-06-30 15:42:15 +0300

    Created src_m4 (last version)

---
+++ examples/2.1.7.3/src_m4/defines.svh
@@ -0,0 +1,19 @@
+`ifndef DEFINES_SVH
+`define DEFINES_SVH
+
+`timescale 1 ns / 1ns
+`default_nettype none 
+
+`ifdef VCS         // Synopsys
+`elsif INCA        // Cadence
+`elsif QUESTA      // Mentor
+`elsif MODEL_TECH  // Mentor
+`else
+  `define NO_COVERAGE
+`endif
+
+`ifndef NO_COVERAGE
+  `define COVERAGE
+`endif
+
+`endif
